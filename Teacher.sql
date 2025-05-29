3fc592ecd767a7daff07b7fa7cd46d8c60825757
shivam singh <shivam998465@gmail.com>
2025-05-29 14:51:09 +0530

    Add files via upload

--- Teacher.sql
+++ Teacher.sql
@@ -1,1 +1,13 @@
-INSERT INTO Teacher
+CREATE DATABASE TEACHER;
+USE  TEACHER;
+
+CREATE TABLE TEACHER(
+  `Teacher_ID` int,
+  `Teacher_Name` varchar(45),
+  `Teacher_Mail_ID` varchar(45),
+  `Teacher_Phone_NO` int ,
+  `Teacher_Attendence_Percentage` int,
+  Course_editor VARCHAR(50),
+  Report_Generator Boolean,
+  ID_of_Student int
+);
