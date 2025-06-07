6c7e23aa14679f28f135845a09a07d9d7244a0a9
shivam singh <shivam998465@gmail.com>
2025-06-07 14:24:00 +0530

    Delete Untitled.sql
    Na

--- Untitled.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Student_Table (Student_Roll_NO, Student_Name, Student_Mail_ID, Student_Marks, Student_Phone_No, Student_No_Of_Enrolled_Course, Student_Report_Generated, Student_Attendance_Percentage) VALUES
--- (1, 'Ram', 'ram@gmail.com', 80, 97237865, 5,1, 93),
--- (2, 'Aditya', 'adity@gmail.com', 80, 98737865, 6,0, 83)
-(3, 'Ram', 'ram@gmail.com', 80, 97237865, 5,1, 93);
-
-Select * from Student_Table;
-
-
